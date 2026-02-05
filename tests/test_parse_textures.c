db07734495410e9b0594fd0b20db793e97c0e5ac
Daniel <daneelahkalgahnov@gmail.com>
2026-02-05 16:05:08 +0100

    fix: correct file path and all checks

--- tests/test_parse_textures.c
+++ tests/test_parse_textures.c
@@ -6,7 +6,7 @@
 /*   By: dkalgano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 11:57:51 by dkalgano          #+#    #+#             */
-/*   Updated: 2026/02/05 13:54:00 by dkalgano         ###   ########.fr       */
+/*   Updated: 2026/02/05 16:03:51 by dkalgano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static void puterror(char *filename, bool result, bool expected) {
 	int	stdout_fd = dup(STDOUT_FILENO);
 	dup2(STDERR_FILENO, STDOUT_FILENO);
 	printf("The test: %s - was failed:\n", filename);
-	printf("Execte result: %d; yours: %d\n", expected, result);
+	printf("Expected result: %d; yours: %d\n", expected, result);
 	dup2(stdout_fd, STDOUT_FILENO);
 	close(stdout_fd);
 }
@@ -30,17 +30,29 @@ static void putsuccess(char *filename) {
 
 int	main(void)
 {
-	char *invalid_test_files[] = {"duplicated_map", NULL};
+	char *invalid_test_files[] = {
+		"duplicated",
+		"empty",
+		"extra_lines",
+		"incomplete_colors",
+		"incomplete_floor",
+		"incomplete_sides",
+		"incomplete_texture_path",
+		"new_lines_only",
+		"wrong_key",
+		NULL
+	};
 	for (int i = 0; invalid_test_files[i] != NULL; i++) {
 		textures_t	textures;
-		char *filename = ft_strjoin("inalid/", invalid_test_files[i]);
+		char *filename = ft_strjoin("tests/examples/textures/invalid/", invalid_test_files[i]);
 		int fd = open(filename, O_RDONLY);
 		bool result = parse_textures(fd, &textures);
 		if (result == true)
-			puterror(filename, result, false);
+			puterror(invalid_test_files[i], result, false);
 		else
-			putsuccess(filename);
+			putsuccess(invalid_test_files[i]);
 		close(fd);
+		free(filename);
 	}
 	return (EXIT_SUCCESS);
 }
