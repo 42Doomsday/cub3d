c95e3d0020d0dfb1d7abb20f729ddc2fb83e686e
Daniel <daneelahkalgahnov@gmail.com>
2026-02-05 15:42:44 +0100

    feat: the prototypes for parsing_textures

---
+++ include/cub3d.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkalgano <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/05 11:54:27 by dkalgano          #+#    #+#             */
+/*   Updated: 2026/02/05 13:48:12 by dkalgano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdbool.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+typedef struct textures_s
+{
+	char *north;
+	char *south;
+	char *west;
+	char *east;
+	int	*floor[3];
+	int	*ceiling[3];
+} textures_t;
+
+bool	parse_textures(int fd, textures_t *out);
+
+#endif
