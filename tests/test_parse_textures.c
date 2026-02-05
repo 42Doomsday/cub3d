0ff22a52c4a05349371aea39f6d30d10caac274d
Daniel <daneelahkalgahnov@gmail.com>
2026-02-05 16:24:14 +0100

    feat: refactoring

--- tests/test_parse_textures.c
+++ tests/test_parse_textures.c
@@ -6,26 +6,48 @@
 /*   By: dkalgano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 11:57:51 by dkalgano          #+#    #+#             */
-/*   Updated: 2026/02/05 16:03:51 by dkalgano         ###   ########.fr       */
+/*   Updated: 2026/02/05 16:22:36 by dkalgano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include "libft.h"
 
-// valgrind --track-fds=yes --leak-check=full --error-exitcode=1 ./a.out
+# define TEXTURES_TESTS_PATH "tests/examples/textures/invalid/"
 
-static void puterror(char *filename, bool result, bool expected) {
+static int	get_test_file(char *filename)
+{
+	char	*path;
+	int		fd;
+
+	path = ft_strjoin(TEXTURES_TESTS_PATH, filename);
+	fd = open(filename, O_RDONLY);
+	free(path);
+	return (fd);
+}
+
+static void	puterror(char *filename, bool result, bool expected)
+{
 	int	stdout_fd = dup(STDOUT_FILENO);
 	dup2(STDERR_FILENO, STDOUT_FILENO);
-	printf("The test: %s - was failed:\n", filename);
+	printf("\nThe test: %s - was failed:\n", filename);
 	printf("Expected result: %d; yours: %d\n", expected, result);
 	dup2(stdout_fd, STDOUT_FILENO);
 	close(stdout_fd);
 }
 
-static void putsuccess(char *filename) {
+static void	putsuccess(char *filename) {
 	printf("Test: %s - passed!\n", filename);
+}
+
+static void	free_struct(textures_t *textures)
+{
+	free(textures->east);
+	free(textures->north);
+	free(textures->south);
+	free(textures->west);
+	free(textures->floor);
+	free(textures->ceiling);
 }
 
 int	main(void)
@@ -42,17 +64,23 @@ int	main(void)
 		"wrong_key",
 		NULL
 	};
-	for (int i = 0; invalid_test_files[i] != NULL; i++) {
-		textures_t	textures;
-		char *filename = ft_strjoin("tests/examples/textures/invalid/", invalid_test_files[i]);
-		int fd = open(filename, O_RDONLY);
-		bool result = parse_textures(fd, &textures);
+
+	textures_t	textures;
+	int			fd;
+	bool		result;
+
+	for (int i = 0; invalid_test_files[i] != NULL; i++)
+	{
+		ft_bzero(&textures, sizeof(textures_t));
+		fd = get_test_file(invalid_test_files[i]);
+		result = parse_textures(fd, &textures);
 		if (result == true)
 			puterror(invalid_test_files[i], result, false);
 		else
 			putsuccess(invalid_test_files[i]);
-		close(fd);
-		free(filename);
+		if (fd > -1)
+			close(fd);
+		free_struct(&textures);
 	}
 	return (EXIT_SUCCESS);
 }
