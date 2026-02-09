3663341610cb499500beb98e827b521748744a4b
Daniel <daneelahkalgahnov@gmail.com>
2026-02-09 18:10:49 +0100

    feat: prototypes

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: dkalgano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 11:54:27 by dkalgano          #+#    #+#             */
-/*   Updated: 2026/02/09 16:46:04 by dkalgano         ###   ########.fr       */
+/*   Updated: 2026/02/09 18:09:45 by dkalgano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,28 @@ typedef struct s_textures
 	int	*ceiling;
 }	t_textures;
 
+typedef struct s_map
+{
+	char	**data;
+	int		height;
+	int		width;
+}	t_map;
+
+typedef enum s_side
+{
+	N,S,E,W
+}	t_side;
+
+typedef struct s_player
+{
+	t_side	side;
+	float	x;
+	float	y;
+}	t_player;
+
 bool	is_valid_path(char *path);
 bool	parse_textures(int fd, t_textures *out);
+bool	parse_map(int fd, t_map *map, t_player *player);
 bool	is_valid_path(char *path);
 
 #endif
