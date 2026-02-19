ab6b767962d4b5bc0af18afccef675fdd30d6d2e
Daniel <daneelahkalgahnov@gmail.com>
2026-02-19 17:29:57 +0100

    feat: in work

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: dkalgano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 11:54:27 by dkalgano          #+#    #+#             */
-/*   Updated: 2026/02/18 14:20:29 by dkalgano         ###   ########.fr       */
+/*   Updated: 2026/02/19 16:57:23 by dkalgano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,12 @@
 # include <stdarg.h>
 
 # include "libft.h"
- #include "MLX42.h"
+# include "MLX42.h"
 
 #define TAB_SIZE 4
 #define VALID_CHARS_MAP "\t\n\v\f\r 01NEWS"
 #define VALID_PLAYER_SIDES "NEWS"
+#define MAP_CHARACTERS "NEWS01"
 #define EXTENSION ".cub"
 
 typedef struct s_textures
@@ -49,6 +50,12 @@ typedef struct s_player
 	float	x;
 	float	y;
 }	t_player;
+
+typedef struct s_coords
+{
+	int	x;
+	int	y;
+}	t_coords;
 
 // parsers
 bool	parse_textures(int fd, t_textures *out);
