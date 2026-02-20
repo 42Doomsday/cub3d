5dc598688fd01594b77b647d8bf04d2ca9202e04
Daniel <daneelahkalgahnov@gmail.com>
2026-02-20 17:49:35 +0100

    fix: error with ft_strlen that was bool due to the wrong operation order

--- tests/test_read_lines.c
+++ tests/test_read_lines.c
@@ -6,7 +6,7 @@
 /*   By: dkalgano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 17:36:29 by dkalgano          #+#    #+#             */
-/*   Updated: 2026/02/18 16:51:52 by dkalgano         ###   ########.fr       */
+/*   Updated: 2026/02/20 17:17:43 by dkalgano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 static bool	compare_str_arrays(char **arr1, char **arr2);
 static void	puterror(char *filename);
 static void	putsuccess(char *filename);
+static void	print_str_array(char **arr);
 
 int	main(void)
 {
@@ -26,13 +27,12 @@ int	main(void)
 		"",
 		"",
 		"         11111111111111",
-		"         1000000E000001"
+		"         1000000E000001",
 		"         10001111110001",
 		"         10001    10001",
 		"         10001111110001",
 		"         10000000000001",
 		"         11111111111111",
-		"",
 		"",
 		"",
 		"",
@@ -44,7 +44,13 @@ int	main(void)
 	if (result)
 		putsuccess("Bagel");
 	else
+	{
 		puterror("Bagel");
+		printf("Yours:\n");
+		print_str_array(res);
+		printf("Expected:\n");
+		print_str_array(exp_res);
+	}
 
 	int	idx = 0;
 	while (res[idx])
@@ -63,7 +69,7 @@ static bool	compare_str_arrays(char **arr1, char **arr2)
 	idx = 0;
 	while (arr1 && arr1[idx])
 	{
-		if (ft_strncmp(arr1[idx], arr2[idx], ft_strlen(arr1[idx]) > 0))
+		if (ft_strncmp(arr1[idx], arr2[idx], ft_strlen(arr1[idx]) + 1) > 0)
 			return (false);
 		idx++;
 	}
@@ -78,4 +84,20 @@ static void	puterror(char *filename)
 static void	putsuccess(char *filename)
 {
 	printf("Test: %s - passed!\n", filename);
+}
+
+
+static void	print_str_array(char **arr)
+{
+	int	i;
+
+	if (!arr)
+		return ;
+	i = 0;
+	while (arr[i])
+	{
+		printf("%s\n", arr[i]);
+		i++;
+	}
+	printf("\n");
 }
