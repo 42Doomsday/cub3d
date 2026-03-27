ffac0b5f3d7250d0b6d0dad23ab1db948e9c5792
Daniel <daneelahkalgahnov@gmail.com>
2026-03-27 18:30:14 +0100

    feat: is it working?

--- src/core/move_player.c
+++ src/core/move_player.c
@@ -12,156 +12,76 @@
 
 #include "cub3d.h"
 
-static t_coords	get_displaced_coords(t_map *map, t_player *player, float degree)
-{
-	t_coords		new;
-	t_coords		wall;
-	t_vec2			normilized;
-	float			radians;
-	float			distance;
-	t_texture_id	side;
-
-	radians = convert_degree_to_radians(degree + player->dir.degree);
-	normilized = normilize(radians);
-	wall = cast_ray_to_wall(player->coords, radians, map);
-	distance = get_dist_to_wall(player->coords, wall);
-	side = get_side_of_wall(wall, normilized);
-	printf("dist %f\n", distance);
-	/* if (distance < PLAYER_HITBOX_R)
-	{
-		printf("enters\n");
-		new.x = 0;
-		new.y = 0;
-		return (new);
-	} */
-	/* if (distance < PLAYER_HITBOX_R)
-	{
-		if (side == NORTH || side == SOUTH)
-		{
-			if (player->dir.unit.y > 0)
-				new.y = (float)((int)new.y + 1) + PLAYER_HITBOX_R;
-			else
-				new.y = (float)(int)new.y - PLAYER_HITBOX_R;
-			new.x = player->coords.x + (normilized.x * PLAYER_R);
-		}
-		else
-		{
-			if (player->dir.unit.x > 0)
-				new.x = (float)(int)new.x - PLAYER_HITBOX_R;
-			else
-				new.x = (float)((int)new.x + 1) + PLAYER_HITBOX_R;
-			new.y = player->coords.y + (normilized.y * PLAYER_R);
-		}
-	}
-	else
-	{
-		new.x = player->coords.x + (normilized.x * PLAYER_R);
-		new.y = player->coords.y + (normilized.y * PLAYER_R);
-	} */
-	new.x = player->coords.x + (normilized.x * PLAYER_R);
-	new.y = player->coords.y + (normilized.y * PLAYER_R);
-	return (new);
-}
-
-/* static float	min(float f, float s)
-{
-	if (f < s)
-		return (f);
-	return (s);
-} */
+static t_coords	get_new_player_position(t_player *player, t_vec2 unit_vector);
+static t_vec2	get_coords_movement(t_vec2 unit_vector, float distance);
+static t_vec2	get_new_unit_vector(t_player *player, float degree_change);
+static float	get_new_rad_dir(t_player *player, float degree_change);
 
 void	move_player(t_map *map, t_player *player, float degree)
 {
-	t_coords	points[3];
-	t_coords	walls[3];
-	float		dists[3];
-	float		radians;
-	float		nearest;
+	t_coords	new;
+	t_vec2		unit_vector;
+	t_coords	wall;
 
-	printf("---------------------\n");
-	points[0] = get_displaced_coords(map, player, degree + -90);
-	points[1] = player->coords;
-	points[2] = get_displaced_coords(map, player, degree + 90);
-
-	/* if ((points[0].x < EPS || points[0].y < EPS))
-		return ;
-
-	if (points[1].x < EPS || points[1].y < EPS)
-		return ;
-
-	if ((points[2].x < EPS || points[2].y < EPS))
-		return ; */
-
-	radians = convert_degree_to_radians(degree + player->dir.degree);
-	walls[0] = cast_ray_to_wall(points[0], radians, map);
-	walls[1] = cast_ray_to_wall(points[1], radians, map);
-	walls[2] = cast_ray_to_wall(points[2], radians, map);
-
-	dists[0] = get_dist_to_wall(points[0], walls[0]);
-	dists[1] = get_dist_to_wall(points[1], walls[1]);
-	dists[2] = get_dist_to_wall(points[2], walls[2]);
-
-	printf("p1 (right) %f %f\n", points[0].x, points[0].y);
-	printf("p2 (center) %f %f\n", points[1].x, points[1].y);
-	printf("p3 (left) %f %f\n", points[2].x, points[2].y);
-
-	printf("dists[0] %f \n", dists[0]);
-	printf("dists[1] %f \n", dists[1]);
-	printf("dists[2] %f \n", dists[2]);
-
-	printf("w1 %f %f\n", walls[0].x, walls[0].y);
-	printf("w2 %f %f\n", walls[2].x, walls[2].y);
-
-	t_texture_id	side;
-	t_vec2			normilized;
-	t_coords		wall;
-
-	normilized = normilize(radians);
-	if ((dists[0] < dists[1]) && (dists[0] < dists[2]))
+	unit_vector = get_new_unit_vector(player, degree);
+	new = get_new_player_position(player, unit_vector);
+	if (is_wall_or_space_on_coords(map, new.x, player->coords.y))
 	{
-		nearest = dists[0] - PLAYER_R / 2;
-		wall = walls[0];
-		side = get_side_of_wall(walls[0], normilized);
-	}
-	else if ((dists[1] < dists[0]) && (dists[1] < dists[2]))
-	{
-		nearest = dists[1];
-		wall = walls[1];
-		side = get_side_of_wall(walls[1], normilized);
-	}
-	else
-	{
-		nearest = dists[2] - PLAYER_R / 2;
-		wall = walls[2];
-		side = get_side_of_wall(walls[2], normilized);
-	}
-
-	printf("neares: %f\n", nearest);
-
-	if (nearest > PLAYER_HITBOX_R)
-	{
-		player->coords.x += (normilized.x * PLAYER_STEP);
-		player->coords.y += (normilized.y * PLAYER_STEP);
-	}
-	else
-	{
-		printf("I should do smth here\n");
-		if (side == NORTH || side == SOUTH)
-		{
-			if (normilized.y < 0)
-				player->coords.y = (float)((int)wall.y) + PLAYER_HITBOX_R;
-			else
-				player->coords.y = (float)(int)wall.y - PLAYER_HITBOX_R;
-			player->coords.x += (normilized.x * PLAYER_STEP);
-		}
+		wall.x = new.x;
+		wall.y = player->coords.y;
+		if (unit_vector.x > 0)
+			new.x = (float)(int)wall.x - PLAYER_HITBOX_R;
 		else
-		{
-			if (normilized.x > 0)
-				player->coords.x = (float)(int)wall.x - PLAYER_HITBOX_R;
-			else
-				player->coords.x = (float)((int)wall.x) + PLAYER_HITBOX_R;
-			player->coords.y += (normilized.y * PLAYER_STEP);
-		}
+			new.x = (float)((int)wall.x + 1) + PLAYER_HITBOX_R;
 	}
-	printf("new coords %f %f\n", player->coords.x, player->coords.y);
+	if (is_wall_or_space_on_coords(map, player->coords.x, new.y))
+	{
+		wall.x = player->coords.x;
+		wall.y = new.y;
+		if (unit_vector.y > 0)
+			new.y = (float)(int)wall.x - PLAYER_HITBOX_R;
+		else
+			new.y = (float)((int)wall.y + 1) + PLAYER_HITBOX_R;
+	}
+	if (is_wall_or_space_on_coords(map, new.x, new.y))
+		return ;
+	player->coords = new;
+}
+
+static t_coords	get_new_player_position(t_player *player, t_vec2 unit_vector)
+{
+	t_coords	new_coords;
+	t_vec2		movement;
+
+	movement = get_coords_movement(unit_vector, PLAYER_STEP);
+	new_coords.x = player->coords.x + movement.x;
+	new_coords.y = player->coords.y + movement.y;
+	return (new_coords);
+}
+
+static t_vec2	get_coords_movement(t_vec2 unit_vector, float distance)
+{
+	t_vec2	movement;
+
+	movement.x = unit_vector.x * distance;
+	movement.y = unit_vector.y * distance;
+	return (movement);
+}
+
+static t_vec2	get_new_unit_vector(t_player *player, float degree_change)
+{
+	t_vec2	unit_vector;
+	float	radians;
+
+	radians = get_new_rad_dir(player, degree_change);
+	unit_vector = normilize(radians);
+	return (unit_vector);
+}
+
+static float	get_new_rad_dir(t_player *player, float degree_change)
+{
+	float	radians;
+
+	radians = player->dir.radians + convert_degree_to_radians(degree_change);
+	return (radians);
 }
